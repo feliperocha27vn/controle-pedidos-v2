3d9d22d0a8e9c74360162b2844a7d8dac76c1453
Felipe <feliperocha.dev27@gmail.com>
2025-07-28 22:18:26 -0300

    feat: trazendo as 10 ultimas vendas

--- src/http/routes/fetch-order-pagination.ts
+++ src/http/routes/fetch-order-pagination.ts
@@ -1,45 +1,28 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
-import z from 'zod'
 import { prisma } from '../../lib/prisma.ts'
 
 export const fetchOrdersPagination: FastifyPluginAsyncZod = async app => {
-  app.get(
-    '/orders/:page',
-    {
-      schema: {
-        params: z.object({
-          page: z.coerce.number(),
-        }),
-      },
-    },
-    async (request, reply) => {
-      const { page } = request.params
-
-      const orders = await prisma.orders.findMany({
-        select: {
-          id: true,
-          customerName: true,
-          quantity: true,
-          totalAmount: true,
-          status: true,
-          createdAt: true,
-          recipe: {
-            select: {
-              title: true,
-            },
+  app.get('/orders', async (_, reply) => {
+    const orders = await prisma.orders.findMany({
+      select: {
+        id: true,
+        customerName: true,
+        quantity: true,
+        totalAmount: true,
+        status: true,
+        createdAt: true,
+        recipe: {
+          select: {
+            title: true,
           },
         },
-        take: 5,
-        skip: (page - 1) * 5,
-      })
+      },
+      orderBy: { createdAt: 'desc' },
+      take: 10,
+    })
 
-      const totalOrder = await prisma.orders.count()
-      const totalPages = Math.ceil(totalOrder / 5)
-
-      reply.status(200).send({
-        orders,
-        totalPages,
-      })
-    }
-  )
+    reply.status(200).send({
+      orders,
+    })
+  })
 }
