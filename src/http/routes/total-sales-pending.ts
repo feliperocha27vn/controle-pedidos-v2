b3564cfbf342cddee2aef9ea8b1b4644b73706c2
Felipe <feliperocha.dev27@gmail.com>
2025-08-09 11:24:49 -0300

    feat: trazendo total de vendas de acordo com o mês

--- src/http/routes/total-sales-pending.ts
+++ src/http/routes/total-sales-pending.ts
@@ -1,14 +1,22 @@
+import { addMonths, startOfMonth } from 'date-fns'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { prisma } from '../../lib/prisma.ts'
 
 export const totalOrdersPending: FastifyPluginAsyncZod = async app => {
   app.get('/orders/pending', async (_, reply) => {
+    const startMonth = startOfMonth(new Date())
+    const endMonth = addMonths(startMonth, 1)
+
     const total = await prisma.orders.aggregate({
       _sum: {
         totalAmount: true,
       },
       where: {
         status: 'pending',
+        createdAt: {
+          gte: startMonth,
+          lt: endMonth,
+        },
       },
     })
 
--- src/http/routes/total-sales.ts
+++ src/http/routes/total-sales.ts
@@ -1,11 +1,21 @@
+import { addMonths, startOfMonth } from 'date-fns'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { prisma } from '../../lib/prisma.ts'
 
 export const totalOrders: FastifyPluginAsyncZod = async app => {
   app.get('/orders/total', async (_, reply) => {
+    const startMonth = startOfMonth(new Date())
+    const endMonth = addMonths(startMonth, 1)
+
     const total = await prisma.orders.aggregate({
       _sum: {
         totalAmount: true,
+      },
+      where: {
+        createdAt: {
+          gte: startMonth,
+          lt: endMonth,
+        },
       },
     })
 
